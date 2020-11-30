7d59435f20638ddf6bc0534448fc9e0d7c3d418c
jason-ruiz <jasonruiz14@explorease.org>
2020-11-30 15:36:04 -0800

    script to upload fits files

--- fits_files.py
+++ fits_files.py
@@ -1,1 +1,20 @@
+# Set up matplotlib and astropy
+import matplotlib.pyplot as plt
+from astropy.io import fits
 
+# Download the fits file
+from astropy.utils.data import download_file
+
+file = 'http://data.astropy.org/tutorials/FITS-images/HorseHead.fits'
+
+image = download_file(file, cache=True)
+
+# simple image grabber
+hdu_list = fits.open(image)
+hdu_list.info()
+new = hdu_list[0].data
+
+# show the image
+plt.imshow(new)
+plt.colorbar()
+plt.show()
